9dbdf5835e050d51b700ea6ca9adc0bf73ea1791
gayatri mane <49158793+gayatrimane@users.noreply.github.com>
2025-08-04 12:45:20 +0530

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -1,2 +1,20 @@
-echo "jenkins sucsefuly"
-echo " this msg is coming from hello.sh file "
+#!/bin/bash
+echo "jenkins build sucesfully"
+echo "current date and time"
+date
+echo
+echo "system update"
+uptime
+echo
+
+echo "harddisk"
+df -h
+echo
+
+echo "memory usage"
+free -m
+echo
+
+echo " list all direcorty anf file"
+ls -l
+
