f9f652282b059ec9c9695381bb3120dd1bd9f3b0
gayatri mane <49158793+gayatrimane@users.noreply.github.com>
2025-08-04 23:20:02 +0530

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 echo "jenkins build sucesfully"
-echo "current date and time"
-date
-echo
-echo "system update"
-uptime
-echo
 
-echo "harddisk"
-df -h
-echo
 
-echo "memory usage"
-free -m
-echo
 
 echo " list all direcorty anf file"
 ls -l
